5e8726fd19e12c47d8cd511d156a9e4c3ce6bd1d
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-06-15 09:51:56 -0300

    mostar no admin

--- Evento/admin.py
+++ Evento/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Evento
+@admin.register()
+class EventoAdmin(admin.ModelAdmin):
+    list_display=('tipo', 'data_inicio', 'data_fim', 'descricao', 'importante')
