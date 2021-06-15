bc6811f3159e0a88239a88f356b20bfa4bd6d4f8
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-06-15 09:54:28 -0300

    nome no admin

--- Evento/admin.py
+++ Evento/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import Evento
-@admin.register()
+@admin.register(Evento)
 class EventoAdmin(admin.ModelAdmin):
     list_display=('tipo', 'data_inicio', 'data_fim', 'descricao', 'importante')
